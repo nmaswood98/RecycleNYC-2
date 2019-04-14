faf9f0384f81f110bff29d455f5c374b538ce688
Nabhan <nmaswood98@gmail.com>
2019-04-14 03:52:46 -0400

    changed the script to get datafrom github and use that to render the visualization

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 
 
-const bouroughJSON = require('../BoroughBoundaries.json'); //Contains the topoJson of the boundaries for the bouroughs
-const recylingBins = require('../RecyclingBinsTopoJson.json'); //Contains the topoJson of the RecylingBinLocation
+//const bouroughJSON = require('../BoroughBoundaries.json'); //Contains the topoJson of the boundaries for the bouroughs
+//const recylingBins = require('../RecyclingBinsTopoJson.json'); //Contains the topoJson of the RecylingBinLocation
 
 
 function createMap() {
@@ -11,26 +11,30 @@ function createMap() {
         .attr("height", 900);
 
 
-    let bouroughs = topojson.feature(bouroughJSON, bouroughJSON.objects.BoroughBoundaries); //converts topojson back into geojson. topojson is better for storage
+        d3.json("https://raw.githubusercontent.com/nmaswood98/RecycleNYC/master/BoroughBoundaries.json", function(error,bouroughJSON){
+            let bouroughs = topojson.feature(bouroughJSON, bouroughJSON.objects.BoroughBoundaries); //converts topojson back into geojson. topojson is better for storage
+            let projection = d3.geoAlbersUsa().fitSize([window.innerWidth, 900], bouroughs); //Create map projection and center it based on the data in the screen
+            let path = d3.geoPath().projection(projection).pointRadius(2.5); //Create geo path using the projection created. 
+        
+            svg.selectAll(".boroughs") //Draw the bouroughs on the screen 
+                .data(bouroughs.features)
+                .enter()
+                .append("path")
+                .attr("class", "borough")
+                .attr("d", path)
+                .attr("fill", '#cccccc');
 
-    let projection = d3.geoAlbersUsa().fitSize([window.innerWidth, 900], bouroughs); //Create map projection and center it based on the data in the screen
-    let path = d3.geoPath().projection(projection).pointRadius(2.5); //Create geo path using the projection created. 
+            
+            d3.json("https://raw.githubusercontent.com/nmaswood98/RecycleNYC/master/RecyclingBinsTopoJson.json",function(error,recylingBins){
 
-    svg.selectAll(".boroughs") //Draw the bouroughs on the screen 
-        .data(bouroughs.features)
-        .enter()
-        .append("path")
-        .attr("class", "borough")
-        .attr("d", path)
-        .attr("fill", '#cccccc');
+                svg.append("path") //Draw each point at a locaiton of a recycling bin
+                .datum(topojson.feature(recylingBins, recylingBins.objects.RecylingBinData))
+                .attr("d", path)
+                .attr("class", "recylingBin")
+                .attr("fill", "green");
 
-    svg.append("path") //Draw each point at a locaiton of a recycling bin
-        .datum(topojson.feature(recylingBins, recylingBins.objects.RecylingBinData))
-        .attr("d", path)
-        .attr("class", "recylingBin")
-        .attr("fill", "green");
-
-
+            });
+        });
 
 }
 
